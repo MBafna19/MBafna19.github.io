7e62536200c10db5a5138f0523d7ee5d399f7e28
Mihir Bafna <reachmihirbafna@gmail.com>
2020-05-13 17:27:36 -0500

    Updates

---
+++ precache-manifest.971f50b8b78be931d5444a6600ca8694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1d7976b119ff14a43a24b1e13d27b2",
+    "url": "/mihirbafna.com/index.html"
+  },
+  {
+    "revision": "6d0f95a28ecaa3fdff93",
+    "url": "/mihirbafna.com/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "642ebfea53fc516b4cae",
+    "url": "/mihirbafna.com/static/js/2.45bafb5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mihirbafna.com/static/js/2.45bafb5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0f95a28ecaa3fdff93",
+    "url": "/mihirbafna.com/static/js/main.bb4a08eb.chunk.js"
+  },
+  {
+    "revision": "a8c4d637f7e5745e54d1",
+    "url": "/mihirbafna.com/static/js/runtime-main.eb88bd8e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03e28df9d6dee22b3396099ade6c9732.js"
+  "/mihirbafna.com/precache-manifest.971f50b8b78be931d5444a6600ca8694.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mihirbafna.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
